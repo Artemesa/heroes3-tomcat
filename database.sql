06880e6627e1303a0d0a311762dc42eedfe57812
Daniel <daniel.j.sotirov@gmail.com>
2023-04-25 16:12:27 +0300

    database

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE heroes3;
+USE heroes3;
+CREATE TABLE potrebiteli(
+id INT,
+name Varchar(255),
+pass Varchar(255),
+mail Varchar(255));
+INSERT INTO potrebiteli VALUES
+    (1, "admin", "admin", "admin@local.bg");
--- src/java/RegisterServlet.java
+++ src/java/RegisterServlet.java
@@ -1,19 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet(urlPatterns = {"/RegisterServlet"})
 public class RegisterServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String r  = """
+                    <!DOCTYPE>
+                    <html>
+                        <head>
+                            <title>Resgister</title>
+                        </head>
+                        <body>
+                            <p>SUIIIIII</p>
+                        </body>
+                    </html>
+                    """;
+        String db = "jdbc:mariadb://localhost/heroes3";
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(db);
+        } catch (SQLException ex) {
+            Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(RegisterServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.getWriter().println(r);
+        
     }
 }
